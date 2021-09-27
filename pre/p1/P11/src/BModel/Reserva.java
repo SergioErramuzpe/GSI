611137ed45e038aca1bc3abe5fad6ed5315bad26
Usama <uboukhebache@gmail.com>
2021-09-27 21:04:02 +0200

    Update Reserva.java

--- pre/p1/P11/src/BModel/Reserva.java
+++ pre/p1/P11/src/BModel/Reserva.java
@@ -12,7 +12,7 @@ import java.util.Date;
 import java.util.Objects;
 
 /**
- *
+ * Clase Reserva
  * @author Usuario
  */
 public class Reserva {
@@ -22,7 +22,18 @@ public class Reserva {
     private Usuario cliente;
     private Reservable reservable;
     private LocalTime lt;
-    
+
+    /**
+     * Constructor de la clase Reserva. A diferencia del constructor siguiente,
+     * este es para reservas que si tienen descuento.
+     * 
+     * @param descuento
+     * @param ld
+     * @param cliente
+     * @param reservable
+     * @param lt
+     */
+
     public Reserva(Float descuento, LocalDate ld, Usuario cliente, Reservable reservable, LocalTime lt) {
         this.descuento = descuento;
         this.cliente = cliente;
@@ -30,7 +41,17 @@ public class Reserva {
         this.ld = ld;
         this.lt = lt;
     }
-    
+
+    /**
+     * Constructor de la clase Reserva. En este caso son para reservas sin
+     * descuento.
+     * 
+     * @param cliente
+     * @param reservable
+     * @param ld
+     * @param lt
+     */
+
     public Reserva(LocalDate ld, Usuario cliente, Reservable reservable, LocalTime lt) {
         this.cliente = cliente;
         this.reservable = reservable;
@@ -40,50 +61,114 @@ public class Reserva {
     
     public Reserva () {}
 
+    /**
+     * Getter del descuento de la reserva
+     * @return
+     */
+
     public Float getDescuento() {
         return descuento;
     }
+
+    /**
+     * Setter del descuento
+     * @param descuento
+     */
 
     public void setDescuento(Float descuento) {
         this.descuento = descuento;
     }
 
+    /**
+     * Getter de la LocalDate de la reserva
+     * @return
+     */
+
     public LocalDate getLd() {
         return ld;
     }
+
+    /**
+     * Setter de LocalDate
+     * @param ld
+     */
 
     public void setLd(LocalDate ld) {
         this.ld = ld;
     }
 
+    /**
+     * Getter del cliente que hace la reserva
+     * @return
+     */
+
     public Usuario getCliente() {
         return cliente;
     }
+
+    /**
+     * Setter de cliente
+     * @param cliente
+     */
 
     public void setCliente(Usuario cliente) {
         this.cliente = cliente;
     }
 
+    /**
+     * Getter de Reservable
+     * @return
+     */
+
     public Local getReservable() {
         return reservable;
     }
+
+    /**
+     * Setter de Reservable
+     * @param reservable
+     */
 
     public void setReservable(Reservable reservable) {
         this.reservable = reservable;
     }
 
+    /**
+     * Getter de LocalTime de la reserva
+     * @return
+     */
+
     public LocalTime getLt() {
         return lt;
     }
+
+    /**
+     * Setter de LocalTime
+     * @param lt
+     */
 
     public void setLt(LocalTime lt) {
         this.lt = lt;
     }
 
+
+    /**
+     * Método toString reescrito sobre la clase Reserva
+     * Sirve para imprimir por pantalla la clase Reserva
+     * @return
+     */
+
     @Override
     public String toString() {
         return "Reserva{" + "descuento=" + descuento + ", ld=" + ld + ", cliente=" + cliente + ", reservable=" + reservable + ", lt=" + lt + '}';
     }
+
+    /**
+     * Override de la función hashCode que nos genera un hashCode hechos 
+     * con los atributos de esta clase (en este caso con LocalDate, cliente,
+     * reservable y LocalTime)
+     * @return
+     */
 
     @Override
     public int hashCode() {
@@ -94,6 +179,15 @@ public class Reserva {
         hash = 67 * hash + Objects.hashCode(this.lt);
         return hash;
     }
+    /**/
+
+    /**
+     * Override al método equals para comparar dos objetos ld (LocalDate), 
+     * cliente, reservable y lt (LocalTime).
+     * 
+     * @param obj
+     * @return
+     */
 
     @Override
     public boolean equals(Object obj) {
