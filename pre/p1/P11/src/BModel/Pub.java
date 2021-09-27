8c05c700e32518e6425176e46ab55b917ebd96cf
Usama <uboukhebache@gmail.com>
2021-09-27 14:17:23 +0200

    Update Pub.java

--- pre/p1/P11/src/BModel/Pub.java
+++ pre/p1/P11/src/BModel/Pub.java
@@ -9,12 +9,22 @@ import java.sql.Time;
 import java.util.Objects;
 
 /**
- *
+ * Clase hija de Local correspondiente a la respresentación de un Pub.
  * @author Usuario
  */
 public class Pub extends Local {
     
     private Time apertura,clausura;
+
+    /**
+     * Cada pub tendrá un nombre, dirección, una descripción y horas de apertura
+     * y clausura.
+     * @param nombreLocal
+     * @param mDireccion
+     * @param descripcion
+     * @param apertura
+     * @param clausura
+     */
 
     public Pub(String nombreLocal, Direccion mDireccion, String descripcion, Time apertura, Time clausura) {
         super(nombreLocal, mDireccion, descripcion);
@@ -22,17 +32,37 @@ public class Pub extends Local {
         this.clausura = clausura;
     }
 
+    /**
+     * El getter de la hora de apertura
+     * @return
+     */
+
     public Time getApertura() {
         return apertura;
     }
+
+    /**
+     * Setter de la hora de apertura
+     * @param apertura
+     */
 
     public void setApertura(Time apertura) {
         this.apertura = apertura;
     }
 
+    /**
+     * Getter de la hora de clausura
+     * @return
+     */
+
     public Time getClausura() {
         return clausura;
     }
+
+    /**
+     * Setter de la hora de apertura
+     * @param clausura
+     */
 
     public void setClausura(Time clausura) {
         this.clausura = clausura;
