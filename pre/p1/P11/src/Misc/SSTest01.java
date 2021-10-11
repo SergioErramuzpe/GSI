3fac838de10dae38b790b3922e60089b952cc950
iazcarate98 <90848140+iazcarate98@users.noreply.github.com>
2021-10-11 23:47:48 +0200

    test02 begining

--- pre/p1/P11/src/Misc/SSTest01.java
+++ pre/p1/P11/src/Misc/SSTest01.java
@@ -7,6 +7,8 @@ package Misc;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import org.jopendocument.dom.OOUtils;
@@ -23,24 +25,38 @@ public class SSTest01 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // TODO code application logic here
-        int[][] array = new int[4][6];
+        
+        //array de datos
+        int array[][] = new int[4][6];
+        
+        //nombre del archivo ods
         final File file = new File("test01.ods");
                 
-        //JTable table = new JTable(4,6);
+        //creamos tabla para los datos
         TableModel table = new DefaultTableModel(4,6);
         
         
         for(int i=0;i<4;i++)
             for(int j=0;j<6;j++){
+                //rellenamos la matriz
                 array[i][j] = (int) Math.floor(Math.random()*11);
+                //Añadir el dato a la tabla
                 table.setValueAt( array[i][j], i, j);
+                //Comprobación de que se ha introducido en la tabla
                 System.out.println(table.getValueAt(i, j));
             }
         
-        SpreadSheet.createEmpty(table).saveAs(file);
-        OOUtils.open(file);
+        try {
+            //cremos la tabla en el archivo
+            SpreadSheet.createEmpty(table).saveAs(file);
+            //abre ods
+            OOUtils.open(file);
+        } catch (IOException ex) {
+            Logger.getLogger(SSTest01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
--- pre/p1/P11/src/Misc/SSTest02.java
+++ pre/p1/P11/src/Misc/SSTest02.java
@@ -5,6 +5,13 @@
  */
 package Misc;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jopendocument.dom.spreadsheet.Sheet;
+import org.jopendocument.dom.spreadsheet.SpreadSheet;
+
 /**
  *
  * @author iazca
@@ -16,6 +23,32 @@ public class SSTest02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //array de datos
+        int array[][] = new int[4][6];
+        
+        //array para celdas
+        String celdas[] ={"D6","E6","F6","G6","H6","I6","D7","E7","F7","G7","H7","I7","D8","E8","F8","G8","H8","I8","D9","E9","F9","G9","H9","I9"};
+        
+        //contador de celdas
+        int countcelda = 0;
+        
+        //guardar test02.ods
+        File file = new File("test02.ods");
+        
+        //abrir test02.ods
+        try {
+            final Sheet sheet = SpreadSheet.createFromFile(file).getSheet(0);
+                } catch (IOException ex) {
+            Logger.getLogger(SSTest02.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        for(int i=0;i<4;i++)
+            for(int j=0;j<6;j++){
+                array[i][j] = (int) Math.floor(Math.random()*11);
+                sheet.getCellAt(countcelda).setValue(array[i][j]);
+                countcelda++;
+            }
     }
     
 }
