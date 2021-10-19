976c3fe232b1d45e5208bc44ba2fa190a71969b1
Oihan Gamio <gamio.112090@e.unavarra.es>
2021-10-19 10:31:18 +0200

    test03

--- pre/p1/P11/src/Misc/SSTest03.java
+++ pre/p1/P11/src/Misc/SSTest03.java
@@ -24,7 +24,7 @@ public class SSTest03 {
     public static void main(String[] args) {
         // TODO code application logic here
         File file = new File("test02.ods");
-        int array[][] = null;
+        int[][] array = new int[4][6];
         SpreadSheet hojaCalculo;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try{
@@ -36,8 +36,8 @@ public class SSTest03 {
             int columna = Integer.parseInt(reader.readLine());
             for(int i = fila; i < 4 + fila; i++){
                 for(int j = columna; j < 6 + columna; j++){
-                    array[j - columna][i - fila] = (int) hojaCalculo.getSheet(0).getCellAt(j, i).getValue();
-                    System.out.println(hojaCalculo.getSheet(0).getCellAt(j, i).getValue());
+                    array[i - fila][j - columna] = Integer.parseInt(hojaCalculo.getSheet(0).getCellAt(j, i).getTextValue());
+                    System.out.println(hojaCalculo.getSheet(0).getCellAt(j, i).getTextValue());
                 }
             }
         }
